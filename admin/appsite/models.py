59815fe224628089d8cfd834a52cd979e59137d8
Rebeca <rebecavelasco2020@gmail.com>
2023-08-22 20:50:57 -0300

    criando Model registrar e transformando em uma migration

--- admin/appsite/models.py
+++ admin/appsite/models.py
@@ -7,3 +7,9 @@ class Site(models.Model):
     description =  models.TextField()
     path = models.ImageField()
     date_create = models.DateTimeField(default=datetime.now, blank =True)
+
+class RegistrarUsuario(models.Model):
+    Email = models.CharField(max_length=200)
+    Senha = models.CharField(max_length=200)
+    date_create = models.DateTimeField(default=datetime.now, blank =True)
+
