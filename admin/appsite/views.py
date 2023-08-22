3239df6af6289d7875c43af54c9526d9c259fb09
Rebeca <rebecavelasco2020@gmail.com>
2023-08-22 19:14:44 -0300

    projeto inicial

---
+++ admin/appsite/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def carrinho(request):
+    return render(request,'carrinho.html')
+
+def produtos(request):
+    return render(request,'produtos.html')
+
+
+
